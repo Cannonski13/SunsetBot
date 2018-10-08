680eb3e7d26e51691770d11952501200046df2bd
Kaznith <43898226+Kaznith@users.noreply.github.com>
2018-10-08 11:57:04 +1100

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,10 +1,6 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 
-bot.on('ready', () => {
-  bot.user.setGame('say help')
-})
-
 client.on('ready', () => {
     console.log('I am ready!');
 });
