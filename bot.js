cb3b20ee9eaa6a02d2c7b315d8b65ed59439e95a
Kaznith <43898226+Kaznith@users.noreply.github.com>
2018-10-08 11:52:17 +1100

    Update bot.js

--- bot.js
+++ bot.js
@@ -6,6 +6,11 @@ client.on('ready', () => {
 });
 
 client.on('message', message => {
+    if (message.content === 'help') {
+    	message.reply('If you are needing help dm @mattyflare') 
+  	}
+});
+client.on('message', message => {
     if (message.content === 'addme') {
     	message.reply('You can add me here: https://discordapp.com/api/oauth2/authorize?client_id=497951381819359233&permissions=0&scope=bot');
   	}
