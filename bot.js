8ed6d4364e79c8994ba5b53922a955712be206a5
Kaznith <43898226+Kaznith@users.noreply.github.com>
2018-10-08 11:24:05 +1100

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,13 +1,19 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 
+client.on("ready",() => {
+  client.user.setGame(prefix + "ping");
+  console.log("SunsetBot is ready for use")  
+});
+
+const prefix = "!" //this is the bots prefix
 client.on('ready', () => {
     console.log('I am ready!');
 });
 
 client.on('message', message => {
-    if (message.content === 'ping') {
-    	message.reply('pong');
+    if (message.content === prefix + 'ping') {
+    	message.reply(prefix + "Pong");
   	}
 });
 
