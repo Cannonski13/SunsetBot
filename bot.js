889946bb4d3e17df38a4f38b6f6f602d735a6909
Kaznith <43898226+Kaznith@users.noreply.github.com>
2018-10-12 14:30:10 +1100

    Update bot.js

--- bot.js
+++ bot.js
@@ -2,20 +2,42 @@ const Eris = require('eris');
 const bot = new Eris(process.env.BOT_TOKEN);
 
 bot.on('ready', () => {
-	console.log('Connected');
-	bot.editStatus('online', {name: 'Version 1.0.1', type :0})
+    console.log('Connected');
+    bot.editStatus('online', {name: 'Version 1.0.1', type :0})
 });
 
 bot.on('messageCreate', msg => {
-	const usedby = `Command ${msg.content} used by ${msg.member.username}#${msg.member.discriminator} (${msg.member.id})`;
+    const usedby = `Command ${msg.content} used by ${msg.member.username}#${msg.member.discriminator} (${msg.member.id})`;
 
-	if (msg.author.bot) return;
+    if (msg.author.bot) return;
 
-bot.createMessage(msg.channel.id, 
-`!addme - You can add me here: https://discordapp.com/api/oauth2/authorize?client_id=497951381819359233&permissions=0&scope=bot
-!updates - A list of updates will be available soon
-!help - If you are needing help then join the support server here: https://discord.gg/zw8Yzhf 
-!commands - Join the support server for the full list of commands `)
+    if (msg.content === '!updates') {
+        bot.createMessage(msg.channel.id, 'Our list of updates can be found in the Official Server (Run command !help to join)')
+        .catch((e) => {console.log(e)});
+
+        console.log(usedby);
+    };
+        
+    if (msg.content === '!commands') {
+        bot.createMessage(msg.channel.id, 'Commands can be found here: https://docs.google.com/document/d/1N9rm2l-dF305MAUNLJp80HhokmWhZ-1dQ2xbm-CMv_s/edit?usp=sharing')
+        .catch((e) => {console.log(e)});
+
+        console.log(usedby);
+    };
+
+    if (msg.content === '!help') {
+        bot.createMessage(msg.channel.id, 'If you are needing help join the support server here: https://discord.gg/zw8Yzhf')
+        .catch((e) => {console.log(e)});
+
+        console.log(usedby);
+    };
+      
+    if (msg.content === '!addme') {
+        bot.createMessage(msg.channel.id, 'You can add me here: https://discordapp.com/api/oauth2/authorize?client_id=497951381819359233&permissions=0&scope=bot')
+        .catch((e) => {console.log(e)});
+
+        console.log(usedby);
+      };
 
 });
 
