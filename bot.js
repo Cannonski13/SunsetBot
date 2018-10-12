e8b9c4852f4776244e134e828cd233c989a45b77
Zoro <swftapocalypse1@gmail.com>
2018-10-11 23:43:06 -0400

    Embed embeds embeds!
    If you wanna add more to it, like a color for example. after the description part you would put
    
    ``color: 0xffffff`` for white
    
    example: 
    
    ``
            bot.createMessage(msg.channel.id, {embed: {
                description: 'If you are needing help join the support server here: https://discord.gg/zw8Yzhf', /* We add the , cause if we don't add it we will get an error, this is an object so we have to seperate every entry with , */
            }})``

--- bot.js
+++ bot.js
@@ -12,28 +12,36 @@ bot.on('messageCreate', msg => {
     if (msg.author.bot) return;
 
     if (msg.content === '!updates') {
-        bot.createMessage(msg.channel.id, 'Our list of updates can be found in the Official Server (Run command !help to join)')
+        bot.createMessage(msg.channel.id, {embed: {
+            description: 'Our list of updates can be found in the Official Server (Run command !help to join)'
+        }})
         .catch((e) => {console.log(e)});
 
         console.log(usedby);
     };
         
     if (msg.content === '!commands') {
-        bot.createMessage(msg.channel.id, 'Commands can be found here: https://docs.google.com/document/d/1N9rm2l-dF305MAUNLJp80HhokmWhZ-1dQ2xbm-CMv_s/edit?usp=sharing')
+        bot.createMessage(msg.channel.id, {embed: {
+            description: 'Commands can be found here: https://docs.google.com/document/d/1N9rm2l-dF305MAUNLJp80HhokmWhZ-1dQ2xbm-CMv_s/edit?usp=sharing'
+        }})
         .catch((e) => {console.log(e)});
 
         console.log(usedby);
     };
 
     if (msg.content === '!help') {
-        bot.createMessage(msg.channel.id, 'If you are needing help join the support server here: https://discord.gg/zw8Yzhf')
+        bot.createMessage(msg.channel.id, {embed: {
+            description: 'If you are needing help join the support server here: https://discord.gg/zw8Yzhf'
+        }})
         .catch((e) => {console.log(e)});
 
         console.log(usedby);
     };
       
     if (msg.content === '!addme') {
-        bot.createMessage(msg.channel.id, 'You can add me here: https://discordapp.com/api/oauth2/authorize?client_id=497951381819359233&permissions=0&scope=bot')
+        bot.createMessage(msg.channel.id, {embed: {
+            description: 'You can add me here: https://discordapp.com/api/oauth2/authorize?client_id=497951381819359233&permissions=0&scope=bot'
+        }})
         .catch((e) => {console.log(e)});
 
         console.log(usedby);
